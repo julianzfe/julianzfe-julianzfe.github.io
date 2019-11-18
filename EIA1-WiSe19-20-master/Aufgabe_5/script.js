564cc41f43d58e2d80ded9e696ee26c3b2791e59
julianzfe <julian.zfe@gmail.com>
2019-11-18 11:34:48 +0100

    1

---
+++ EIA1-WiSe19-20-master/Aufgabe_5/script.js
@@ -0,0 +1,11 @@
+var test = "hallo";
+var welt = "welt";
+var zahl = 90;
+var zahl2 = 10;
+console.log(zahl2 * zahl);
+var zahl = 20;
+var zahl1 = 200;
+var ergebnis = (zahl / zahl1) * 100;
+var prozent = "%";
+console.log(ergebnis + " %");
+//# sourceMappingURL=script.js.map
