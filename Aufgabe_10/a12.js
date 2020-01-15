77b26d145b11da4431c3a9e4e8a30ceeecb9d843
julianzfe <julian.zfe@gmail.com>
2020-01-15 16:56:11 +0100

    v

---
+++ Aufgabe_10/a12.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=a12.js.map
--- EIA1-WiSe19-20-master/Aufgabe_5/testordner/script.js
+++
@@ -1,5 +0,0 @@
-var emissionAsia = 1000;
-var emissionEurope = 100;
-var totalEmission = emissionAsia + emissionEurope;
-console.log("Die Gesamtemission ist: " + totalEmission);
-//# sourceMappingURL=script.js.map
