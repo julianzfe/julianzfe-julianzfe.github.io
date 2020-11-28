6aee367283f2338ce7f5a18132032a64592a3b0e
Ziefle_Julian <julian.zfe@gmail.com>
2020-11-28 15:49:03 +0100

    a7

---
+++ OT/RandomBoi/RandomBoi.js
@@ -0,0 +1,9 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+    console.log(getRandomInt(10));
+}
+console.log(getRandomInt(10));
+window.addEventListener('load', function () {
+    document.querySelector(".roll").addEventListener('click', getRandomInt);
+});
+//# sourceMappingURL=RandomBoi.js.map
